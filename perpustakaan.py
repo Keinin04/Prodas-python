0e518eb6c313623d710b8405ca278729d45b018d
Rifqy P <60067877+Keinin04@users.noreply.github.com>
2022-12-04 08:26:58 +0700

    Update function
    menggunakaan fungsi pandas & ditambahkan object class

--- perpustakaan.py
+++ perpustakaan.py
@@ -1,48 +1,28 @@
 #   Applikasi perpustakaan
 
 import os
-
-def clear_screen():
-    os.system('cls')
-
-def kembali():
-    print("\n")
-    input("Tekan tombol apa saja untuk kembali...")
-    clear_screen()
-
-buku = ['Henry book', 'Harry potter 1', 'buku cerdas cermat']
-
-def show_data():
-    if len(buku) <= 0 :
-        print("Tidak ada Buku")
-    else:
-        for i in range(len(buku)):
-            print('%i %s' %(i, buku[i]))
-
-def insert_data(): # Menambahkan Buku
-    while(True):
-        buku_baru = input('Judul buku: ')
-        buku.append(buku_baru)
-        while(True):
-            option = (input("Ingin menambahakan lagi? [y/n]: "))
-
-            if option == 'iya' or option == 'ya' or option == 'y':
-                clear_screen()
-                break
-            elif option == 'n':
-                clear_screen()
-                continue
-            else:
-                print('Masukan n atau y')
-                kembali()
+import datetime
+import pandas as pd
 
 
 
-    
+class Menu ():
+    def clear_screen(): # untuk menghapus tampilan
+        os.system('cls')
 
+    def kembali():
+        print("\n")
+        input("Tekan tombol apa saja untuk kembali...")
+        Menu.clear_screen()
 
+    def getDate(): # untuk mendapatkan tanggal
+        now = datetime.datetime.now
+        return str(now().date())
 
-class Menu:
+    def getTime(): # untuk mendapatkan waktu 
+        now = datetime.datetime.now
+        return str(now().time())
+
     def menu_pertama():
         while(True):
             print("     Menu Perpustakaan   ")
@@ -51,24 +31,105 @@ class Menu:
             print(" 2 Untuk Pinjamkan Buku")
             print(" 3 Untuk Kembalikan Buku")  
             print(" 4 Untuk Tambahkan Buku")
-            print(" 5 Untuk Keluar") 
             try:
                 p=int(input('pilih menu 1-5: '))
                 print()
                 if p == 1:
-                    clear_screen()
-                    show_data()
-                    kembali()
+                    Menu.clear_screen()
+                    Buku.menampilkan_buku()
+                    Menu.kembali()
+                elif p == 2:
+                    Menu.clear_screen()
+                    Buku.listBuku()
+                    Buku.minjam()
                 elif p == 4:
-                    clear_screen()
-                    insert_data()
+                    Buku.listBuku()
+                    Menu.clear_screen()
                 else:
                     print('Masukkan angka')
-                    kembali()
+                    Menu.kembali()
                     continue
             except ValueError:
                 print('Masukan pilihan sesuai nomor !')
-                kembali()
+                Menu.kembali()
                 continue
+
+class Buku():
+
+    def listBuku():
+        global judul_buku
+        global pengarang
+        global jumlah_stok
+        global harga
+
+        judul_buku = []
+        pengarang = []
+        jumlah_stok = []
+        harga = []
+
+        with open(r'Prodas-python\buku.txt','r+') as f:
+
+            lines = f.readlines() # membaca baris 
+            lines = [x.strip('\n') for x in lines] # menghapus newline
+            for i in range(len(lines)):
+                ind = 0
+                for a in lines[i].split(','):
+                    if(ind==0):
+                        judul_buku.append(a)
+                    if(ind==1):
+                        pengarang.append(a)
+                    if(ind==2):
+                        jumlah_stok.append(a)
+                    if(ind==3):
+                        harga.append(a)
+                    ind+=1
+
+    def menampilkan_buku(): # Menampilkan Data buku
+        Buku.listBuku()
+        buku = {
+            'Judul Buku' : judul_buku,
+            'Nama Pengarang' : pengarang,
+            'Jumlah stok' : jumlah_stok,
+            'harga' : harga,
+        }
+
+        daftar_buku = pd.DataFrame(buku)
+        print('====================================================================================================')
+        print(daftar_buku)
+        print('====================================================================================================')
+            
+
+    def minjam(): # peminjaman buku 
+        success = False
+        while(True):
+            firstName = input('Masukkan nama depan peminjam: ')
+            if firstName.isalpha(): # akan dijalankan jika typedatanya str
+                break
+            print('Masukkan huruf')
+        while(True):
+            lastName= input('Masukkan nama belakang peminjam: ')
+            if lastName.isalpha(): # akan dijalankan jika typedatanya str
+                break
+            print('Masukkan huruf')
+        Buku.menampilkan_buku()
+
+        t = 'Prodas-python' + '\\' + 'Pinjaman-'+firstName+'.txt'
+        with open(t,'w+') as f:
+            f.write('           Perpustakaan \n')
+            f.write('       Dipinjam oleh: '+ firstName + ' ' + lastName + '\n')
+            f.write('    Tanggal: ' + Menu.getDate()+'    Waktu:'+ Menu.getTime()+'\n\n')
+            f.write('S.N. \t\t Judul buku \t      Pengarang \n' )
+
+        while success == False:
+            print('Pilih menu di bawah ini :')
+            for i in range(len(judul_buku)):
+                print('Masukkan', i, 'untuk meminjam buku', judul_buku[i])
+
+
+
+    
+
+
+
 
 Menu.menu_pertama()
